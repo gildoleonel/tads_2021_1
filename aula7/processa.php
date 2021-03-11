9286039e821f165c4e1034cf6ab68f2612ad3273
Gildo Leonel <gleonel@viannajunior.edu.br>
2021-03-10 21:46:30 -0300

    Exercício aula 7
    Base para o exercício

---
+++ aula7/processa.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+echo 'O sobrenome do usuário é: '.$_POST['nome']. '<BR>';
+echo 'O turno dele é '.$_POST['turno'].'<BR>';
+echo 'O periodo é '.$_POST['periodo'].'<BR>';
+echo 'Estava presente nas aulas: ';
+
+    * primeira
+    * segunda
+    * terceira
+    * quarta
+
+getNomeAula($numAula);
